9cf6b4477e17c0c8437ae7ea50c78fd314ffedcd
Arthur Bacci <arthurbacci@protonmail.com>
2023-12-30 02:40:57 -0300

    Basic display loop

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,33 @@
-use std::fs::File;
-use std::io::Read;
-use std::collections::HashMap;
+/*
+ *  FIXME: there seems to be an issue with the graphics library, that sometimes
+ *  fails transmitting images. Testing of its output must be done.
+ *
+ *  Next: Library for kitty's keyboard protocol (Rust or C).
+ *  Then: Add some standard escape sequences to the graphics library
+ *  And then: implement a basic visual mode
+ */
+
+
 use std::sync::mpsc::TryRecvError;
 
 use piecetedrevival::bind;
-use piecetedrevival::graphics::{KittyImage, KittyImageCmdValue};
 
 fn main() {
     let rx = bind::get_sigwinch_channel();
-    println!("{:?}", bind::get_window_sz());
 
+    let mut window_sz = bind::get_window_sz();
 
-    let mut logo = Vec::new();
-    File::open("data/logo.png").unwrap().read_to_end(&mut logo).unwrap();
+    loop {
+        match rx.try_recv() {
+            Err(TryRecvError::Empty) => {}
+            Err(err) => panic!("This error happened: {err}"),
+            Ok(()) => {
+                window_sz = bind::get_window_sz();
+            }
+        }
 
-    let logo = KittyImage::new(logo, HashMap::from([
-        ('f', KittyImageCmdValue::U(100)),
-    ]));
-
-
-    let mut c = Vec::new();
-    File::open("data/c.png").unwrap().read_to_end(&mut c).unwrap();
-
-    let c = KittyImage::new(c, HashMap::from([
-        ('f', KittyImageCmdValue::U(100)),
-    ]));
-
-    let c1 = c.place(HashMap::from([
-        ('y', KittyImageCmdValue::U(413)),
-        ('c', KittyImageCmdValue::U(32)),
-        ('r', KittyImageCmdValue::U(16)),
-        ('z', KittyImageCmdValue::I(-2)),
-        ('C', KittyImageCmdValue::U(1)),
-    ]));
-
-    let l1 = logo.place(HashMap::from([
-        ('c', KittyImageCmdValue::U(16)),
-        ('r', KittyImageCmdValue::U(8)),
-    ]));
-
-    let l2 = logo.place(HashMap::from([
-        ('c', KittyImageCmdValue::U(16)),
-        ('r', KittyImageCmdValue::U(8)),
-    ]));
+    }
 
 
 
-    while let Err(TryRecvError::Empty) = rx.try_recv() {}
-
-    drop(c1);
-    drop(l1);
-    drop(l2);
 }
